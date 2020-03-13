22d888d2248f9eb75d9188df3719c1c99431832c
duanning <duanning@gome.com.cn>
2020-03-13 10:51:59 +0800

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    title:'Hello VuePress',
+    base:'/gfe/gwiki',
+    dest:'public',
+    themeConfig: {
+        sidebar: [
+            '/',
+            '/api/cli',
+            ['/api/node', 'Explicit link text']
+          ],
+        nav: [
+            {
+                text: 'Languages',
+                ariaLabel: 'Language Menu',
+                items: [
+                    {
+                        text: 'Group1',
+                        items: [
+                            {text: 'Chinese', link: '/language/chinese/'},
+                            { text: 'Japanese', link: '/language/japanese/'}
+                        ]
+                    },
+                    {
+                        text: 'Group2', 
+                        items: [
+                            { text: 'Chinese', link: '/language/chinese/' },
+                            { text: 'Japanese', link: '/language/japanese/' }
+                        ]
+                    }
+        ]
+    }],
+        logo: '/assets/img/logo-new.png',
+}
+}
